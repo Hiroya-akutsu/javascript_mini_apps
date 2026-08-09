ab0e31f45661cbc94fc1c90825177a78ca3d2d5d
maybehiro <89362941+Hiroya-akutsu@users.noreply.github.com>
2026-08-09 22:50:12 +0900

    style: トップページ更新

---
+++ script.js
@@ -0,0 +1,26 @@
+const projects = [
+  {
+    mark: "数",
+    label: "01 / Find the number",
+    meta: "JavaScript / Game",
+    title: "Number Guess",
+    description: "1〜10の中から選ばれた数字を当てるゲーム。",
+    url: "./number_guess/"
+  },
+  {
+    mark: "運",
+    label: "02 / Try your luck",
+    meta: "JavaScript / Game",
+    title: "Omikuji",
+    description: "ボタンを押して運勢を占うミニアプリ。",
+    url: "./omikuji/"
+  },
+  {
+    mark: "拳",
+    label: "03 / Rock, scissors, paper",
+    meta: "JavaScript / Game",
+    title: "Janken",
+    description: "シンプルなじゃんけんアプリ",
+    url: "./janken/",
+  }
+]
