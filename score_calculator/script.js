47e7a4c2c6c0f1d6bf9b649ff566f448095702c1
maybehiro <89362941+Hiroya-akutsu@users.noreply.github.com>
2026-08-22 19:51:07 +0900

    feat: 成績表完成

--- score_calculator/script.js
+++ score_calculator/script.js
@@ -1,12 +1,30 @@
 // フォームの値を取得
 const formElement = document.getElementById('score_calculator');
 
-formElement.addEventListener('submit', () => {
+// 結果表示欄の要素を取得
+const result = document.getElementById('result');
+
+formElement.addEventListener('submit', (event) => {
   event.preventDefault();
   // 入力値を全て取得
   const subjectScores = document.querySelectorAll('[data-subject]');
 
+  // スコアを初期化
+  let score = 0; 
+
   for (const subjectInput of subjectScores) {
-    const score = Number(subjectInput.value);
+    const scoreInput = Number(subjectInput.value);
+    score += scoreInput;
+  };
+
+  // 成績の判定
+  if (score >= 240) {
+    result.textContent = 'A'
+  } else if (score >= 180) {
+    result.textContent = 'B'
+  } else if (score >= 120) {
+    result.textContent = 'C'
+  } else {
+    result.textContent = 'D'
   };
 });
